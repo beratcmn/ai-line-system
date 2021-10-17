71203f03c7b28d9e8b10a694951de08ee85dd831
Berat Cimen <beratcmn@gmail.com>
2021-10-17 23:32:05 +0300

    Rewamped the scan method

--- User.py
+++ User.py
@@ -4,15 +4,16 @@ from dataclasses import dataclass
 @dataclass
 class User:
     name: str
+    surname: str
     id: str
 
     placeInLine = 0
     remainingTime = 0
 
-    def renewTime(self):
+    def RenewTime(self):
         self.remainingTime = 1200  # saniye cinsinden kalan süre
 
-    def decreaseTime(self):
+    def DecreaseTime(self):
         self.remainingTime = self.remainingTime - 1
 
         # TODO her 3 dakika da bir bütün kullanıcıları kontrol edip sürelerini azalt
