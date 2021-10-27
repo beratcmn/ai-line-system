f6b11d4b0dbbc0a43656b040c7bc0cf81da1e514
Berat Çimen <beratcmn@gmail.com>
2021-10-27 12:28:49 +0300

    Added the Class Structure

---
+++ Class/User.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    name: str
+    surname: str
+    id: str
+
+    placeInLine = 0
+    remainingTime = 0
+
+    def RenewTime(self):
+        self.remainingTime = 1200  # saniye cinsinden kalan süre
+
+    def DecreaseTime(self):
+        self.remainingTime = self.remainingTime - 1
